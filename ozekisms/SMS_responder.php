1ced8ff35c490f36895f6cf41b926881059fa539
cntabana <cntabana@yahoo.fr>
2014-09-25 16:45:49 +0100

    sms

--- ozekisms/SMS_responder.php
+++ ozekisms/SMS_responder.php
@@ -8,29 +8,35 @@ $msg = $_GET['msg'];
 
 $valuearray=explode("*",$msg);
 $service = $valuearray[0];
-$email   = $valuearray[1];
-$request = $valuearray[2];
-$message = "";
-$date    = date('Y-m-d');
-$phone   = $_GET['sender'];
+
+
+
+$con = mysqli_connect("localhost","root","","psfdb");
 
 	if(strtolower($service) == "kubaza")
 	{
+            $email   = $valuearray[1];
+			$request = $valuearray[2];
+			$message = "";
+			$date    = date('Y-m-d');
+			$phone   = $_GET['sender'];
 
 			if($email!="" and $request!="") //if($district!="" and $product!=""$)
 				{
 				//query from database 
 				    $query="insert into request(request,phonenumber,email,requestdate,status) values ('".$request."','".$phone."','".$email."','".$date."',0)";
-					$con = mysqli_connect("localhost","root","","psfdb");
 					$rs = mysqli_query($con,$query);
 					if($rs){
-						$message = "Twishimiye ikibazo cyanyu,tuzabasubiza vuba. Murakoze.";
+						$message = "Twishimiye ikibazo cyanyu,tuzabasubiza vuba. Murakoze." ;
+						$queryId = "SELECT id FROM request WHERE request =".$message;
+						echo $queryId;
+						//$rs = mysqli_query($con,)
 					}else{
 
 						$message = "........ error";
 					}
 
-				    mysqli_close($con);
+
 
 				}
 			else
@@ -41,33 +47,40 @@ $phone   = $_GET['sender'];
 	}
 	else if(strtolower($service) == "igisubizo"){
 
-                if($code !="") 
+                $idrequest = $valuearray[1];
+
+                if($idrequest !="") 
                 {
 				
-				$query="select status from request where code =".$code;
-					
-                $con = mysqli_connect("localhost","inyungu_inyungu","QHTJ-h^5mAQH","inyungu_inyungudb");
-				//$db_selected = mysql_select_db("inyungu_inyungudb", $con);
-					
-				$rs = mysqli_query($con,$query);
-
+				$query="select status,email from request where id =".$idrequest;
+									
+                $rs = mysqli_query($con,$query);
 				//end of query from database
 				while($row = mysqli_fetch_array($rs))
 				{
-						
-			     $message=$message." ".$row['productname']."  ".$row['quantity'];
+				
+				 if($row['status'] == 0){
+                       	$message = "Pending";
+				 }
+				 else  if($row['status'] == 1){
+                      $message = "Open";
+				 }
+				 else  if($row['status'] == 2){
+                      $message = "Under Process";
+				 }else{
+				 	
+					  $message = "Dossier yanyu yararangiye,mushobora kureba igisubizo muri email ".$row['email'];
+
+				 }
+			     
 				    
 			   }
+			}
+			else
+			{
+				$message='wanditse nabi !!! andika "igisubizo*idrequest" murakoze.';
 
-
-
-
-
-	}
-	else
-	{
-		$message='wanditse nabi !!! andika "kubaza*email*request" murakoze.';
-
+			}
 	}
 	
 	
@@ -75,5 +88,5 @@ echo "{SMS:TEXT}{}{+250788543310}{".$_GET['sender']."}{".$message."}";
 
 }
 
-
+mysqli_close($con);
 ?>
